2f30f65309ca5193ecdc47a3fc35f14bebf35eee
3CN-O4D <3cno4d@gmail.com>
2026-08-14 20:45:28 +0300

    Remove old Python zen directory, keep only native Rust zen-runtime

--- zen/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def read_file(path):
-    path = os.path.expanduser(path)
-    with open(path, 'r', encoding='utf-8') as f:
-        return f.read()
-
-def resolve_path(path):
-    return os.path.abspath(os.path.expanduser(path))
